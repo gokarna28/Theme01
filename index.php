676fb9ccb27286107b4d723b358451bcb53eb901
Gokarna Chaudhay <gokarnachy28@gmail.com>
2024-07-17 16:22:17 +0545

    the_post(); have_posts();

--- index.php
+++ index.php
@@ -3,7 +3,25 @@
 get_header();
 ?>
 <div class="content">
-    
+
+</div>
+
+
+<!-- post section -->
+ <div class="post_wrapper">
+<?php
+while (have_posts()) {
+    the_post();//counting the post and set the limit
+    ?>
+    <div class="post_container">
+        <img src="<?php echo get_template_directory_uri(); ?>/image/post.jpg">
+        <h1><?php the_title();?></h1>
+        <p>Discription</p>
+        <p><?php the_date();?></p>
+    </div>
+    <?php
+}
+?>
 </div>
 <?php
 // Including WordPress footer
